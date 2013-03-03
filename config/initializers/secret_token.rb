6f200892e6f5495998663d24515d172a15eb02ac
Sam Bao <sam.bao@cerner.com>
2013-03-02 19:12:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '373827be8a4593121f5e85629be54cda5e1623e5d1fdab6ba749e374d2f45f2b523c47b50f904c02c81ddaf1a10dbbc4bcfe10aed24c61ae355fd6d5d7805c60'
